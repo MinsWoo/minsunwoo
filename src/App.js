76005ce5d1925b08d8b59435cb1171870b23ec91
MinsWoo <89885078+MinsWoo@users.noreply.github.com>
2024-07-01 19:23:38 +0900

    react-study

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      flex: 1,
+      textAlign: 'center',
+      flexDirection: 'column',
+      justifyContent: 'center',
+    }}>
+      <span>
+        test
+      </span>
+      <img alt='image' src='https://exqibabapqbf17923502.cdn.ntruss.com/front/kr/order/icon_order_bottomGuide.png' 
+       style={{
+        width: '329px',
+        height: '90px'
+       }}
+      />
     </div>
   );
 }
