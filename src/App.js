f5b3530f3b1a977c7eff55a4d207041349405297
MinsWoo <wooms101@gmail.com>
2024-07-02 19:01:39 +0900

    practice2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/home/Home.tsx';
 
 function App() {
   return (
-    <div style={{
-      flex: 1,
-      textAlign: 'center',
-      flexDirection: 'column',
-      justifyContent: 'center',
-    }}>
-      <span>
-        test
-      </span>
-      <img alt='image' src='https://exqibabapqbf17923502.cdn.ntruss.com/front/kr/order/icon_order_bottomGuide.png' 
-       style={{
-        width: '329px',
-        height: '90px'
-       }}
-      />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import './css/Home.css';
+
+
+export const Home = () => {
+    return (
+        <div className='main-container'>
+            <div className='header-container'>
+                <div className='logo-image'>
+                </div>
+                <div className='header-menu'>
+                    <button>
+                        <span>home</span>
+                    </button>
+                    <button>
+                        <span>home</span>
+                    </button>
+                    <button>
+                        <span>home</span>
+                    </button>
+                </div>
+            </div>
+            <div className='mid-content'>
+                <span style={{
+                    fontWeight: 'bold',
+                    fontSize: '24px',
+                }}>타이틀</span>
+                <span>내용</span>
+            </div>
+            <div className='image-container'>
+
+                <img
+                    alt='image'
+                    src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fgongu.copyright.or.kr%2Fgongu%2Fwrt%2Fwrt%2Fview.do%3FwrtSn%3D9046601%26menuNo%3D200018&psig=AOvVaw3Br0X_wllIkTnJYxy3nsSw&ust=1720000314139000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMiej9CKiIcDFQAAAAAdAAAAABAE'
+                    style={{
+                        width: '80%',
+                        height: '300px',
+                        backgroundColor:'grey'
+                    }}
+                />
+                                <img
+                    alt='image'
+                    src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fgongu.copyright.or.kr%2Fgongu%2Fwrt%2Fwrt%2Fview.do%3FwrtSn%3D9046601%26menuNo%3D200018&psig=AOvVaw3Br0X_wllIkTnJYxy3nsSw&ust=1720000314139000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMiej9CKiIcDFQAAAAAdAAAAABAE'
+                    style={{
+                        width: '80%',
+                        height: '300px',
+                        backgroundColor:'grey'
+                    }}
+                />
+                                <img
+                    alt='image'
+                    src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fgongu.copyright.or.kr%2Fgongu%2Fwrt%2Fwrt%2Fview.do%3FwrtSn%3D9046601%26menuNo%3D200018&psig=AOvVaw3Br0X_wllIkTnJYxy3nsSw&ust=1720000314139000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMiej9CKiIcDFQAAAAAdAAAAABAE'
+                    style={{
+                        width: '80%',
+                        height: '300px',
+                        backgroundColor:'grey'
+                    }}
+                />
+                                <img
+                    alt='image'
+                    src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fgongu.copyright.or.kr%2Fgongu%2Fwrt%2Fwrt%2Fview.do%3FwrtSn%3D9046601%26menuNo%3D200018&psig=AOvVaw3Br0X_wllIkTnJYxy3nsSw&ust=1720000314139000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMiej9CKiIcDFQAAAAAdAAAAABAE'
+                    style={{
+                        width: '80%',
+                        height: '300px',
+                        backgroundColor:'grey'
+                    }}
+                />
+            </div>
+        </div>
+    )
+}
